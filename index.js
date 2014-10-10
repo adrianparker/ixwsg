f3f372a205af12d098bf9af05acbbe5f1fb0ce30
Adrian Parker <adrian_parker@bnz.co.nz>
2014-10-10 15:12:54 +1300

    Moar tests

--- index.js
+++ index.js
@@ -3,45 +3,53 @@
  *
  * Also removes any leading or trailing whitespace, that is, whitespace before the first < or after the last >.
  * Presumes XML is well-formed and is demarcated by < and > characters.  If you pass partial XML nodes then
- * you may experience data loss. But you wouldn't do that now would you.
+ * you may experience data loss. But you wouldn't do that now would you. No. No you would not.
  *
  * Created by Adrian Parker on 3/10/14.
  *
- *
  */
 module.exports = {
-    gobble: function (xml) {
-		var debugCtr = 0;
-        // remove any leading or trailing chars indiscriminately
-		console.log(++debugCtr+' '+xml);
-        xml = String(xml).substring(xml.indexOf('<'));
-		console.log(++debugCtr+' '+xml);
-		var l1 = xml.lastIndexOf('>');
-		if(l1 > -1) {
-        	xml = xml.substring(0, l1)+'>';
+	gobble: function (xml) {
+		var outXml = '',
+			lastGreaterThanIndex = -1,
+			firstGreaterThanIndex = -1,
+			subsequentLessThanIndex = -1,
+			trimmedGap = 0,
+			possibleWhitespace = '';
+
+		// remove any leading or trailing chars indiscriminately
+		xml = String(xml).substring(xml.indexOf('<'));
+		lastGreaterThanIndex = xml.lastIndexOf('>');
+		if (lastGreaterThanIndex > -1) {
+			xml = xml.substring(0, lastGreaterThanIndex) + '>';
 		}
-		console.log(++debugCtr+' '+xml);
-        //walk the remainder, find the chars between each > and <
-        var endLeft = getLeft(xml);
-        var beginRight = getRight(xml, endLeft);
-        while (endLeft > -1 && beginRight > -1 && endLeft < beginRight) {
-            var possibleWS = xml.substring(endLeft, beginRight);
-            if (possibleWS.trim() === '') {
-                xml = xml.substring(0, endLeft) + xml.substring(beginRight);
-				console.log(++debugCtr+' '+xml);
-            }
-            endLeft = getLeft(xml);
-            beginRight = getRight(xml, endLeft);
-        }
 
-        function getLeft(xml) {
-            return String(xml).indexOf('>');
-        }
+		// walk the remainder of the text,
+		// find the chars between each > and <,
+		// examine for whitespace and gobble if so
+		firstGreaterThanIndex = getLeft(xml);
+		subsequentLessThanIndex = getRight(xml, firstGreaterThanIndex);
+		while (firstGreaterThanIndex > -1 && subsequentLessThanIndex > -1 && firstGreaterThanIndex < subsequentLessThanIndex) {
+			trimmedGap = 0;
+			possibleWhitespace = xml.substring(firstGreaterThanIndex + 1, subsequentLessThanIndex);
+			if (possibleWhitespace.trim() === '') {
+				xml = xml.substring(0, firstGreaterThanIndex + 1) + xml.substring(subsequentLessThanIndex);
+				trimmedGap = subsequentLessThanIndex - (firstGreaterThanIndex + 1);
+			}
+			outXml += xml.substring(0, subsequentLessThanIndex - trimmedGap);
+			xml = xml.substring(subsequentLessThanIndex - trimmedGap);
+			firstGreaterThanIndex = getLeft(xml);
+			subsequentLessThanIndex = getRight(xml, firstGreaterThanIndex);
+		}
 
-        function getRight(xml, endLeft) {
-            return String(xml).substring(endLeft).indexOf('<') + endLeft;
-        }
+		function getLeft(xml) {
+			return String(xml).indexOf('>');
+		}
 
-        return xml;
-    }
+		function getRight(xml, endLeft) {
+			return String(xml).substring(endLeft).indexOf('<') + endLeft;
+		}
+
+		return outXml + xml;
+	}
 }
--- test/index.js
+++ test/index.js
@@ -1,26 +1,50 @@
 var should = require('chai').should(),
-    ixwsg = require('../index'),
-    gobble = ixwsg.gobble;
+	ixwsg = require('../index'),
+	gobble = ixwsg.gobble;
 
 describe('#gobble', function () {
-    it('strips whitespace before first &lt; char', function () {
-        gobble('     <xml').should.equal('<xml');
-    });
-    it('strips non-whitespace before first &lt; char', function () {
-        gobble('  fd   <xml').should.equal('<xml');
-    });
-    it('strips non-whitespace after last &gt; char', function () {
-        gobble('<> dh rdb 45 ').should.equal('<>');
-    });
-    it('strips whitespace after last &gt; char', function () {
-        gobble('<xml/>               ').should.equal('<xml/>');
-    });
+	it('strips whitespace before first < char', function () {
+		gobble('     <xml').should.equal('<xml');
+	});
+	it('strips non-whitespace before first , char', function () {
+		gobble('  fd   <xml').should.equal('<xml');
+	});
+	it('strips non-whitespace after last > char', function () {
+		gobble('<> dh rdb 45 ').should.equal('<>');
+	});
+	it('strips whitespace after last > char', function () {
+		gobble('<xml/>               ').should.equal('<xml/>');
+	});
 	it('should not modify a string with no < or > characters in it', function () {
 		gobble('The quick brown fox').should.equal('The quick brown fox');
 	});
-
-//	it('removes whitespace from within an XML documents nodes', function () {
-//		gobble('<xml><abc>dont remove this</abc>	<def>   nor this </def>  <ghi> </ghi>	  </xml>').should.equal('<xml><abc>dont remove this</abc><def>   nor this </def><ghi></ghi></xml>');
-//	});
-
+	it('removes mixed tab and space whitespace', function () {
+		gobble('<xml><abc>dont remove this</abc>	<def>   nor this </def>  <ghi> </ghi>	  </xml>').should.equal('<xml><abc>dont remove this</abc><def>   nor this </def><ghi></ghi></xml>');
+	});
+	it('removes tab only whitespace', function () {
+		gobble('\t<xml>\t<a>\t<b>\t</b>\t</a><c>\t<d>\t</d></c>\t\t\t</xml>\t\t').should.equal('<xml><a><b></b></a><c><d></d></c></xml>');
+	});
+	it('removes line feed only whitespace', function () {
+		gobble('\n<xml>\n<a>\n<b>\n</b>\n</a><c>\n<d>\n</d></c>\n\n\n</xml>\n\n').should.equal('<xml><a><b></b></a><c><d></d></c></xml>');
+	});
+	it('removes carriage return only whitespace', function () {
+		gobble('\r<xml>\r<a>\r<b>\r</b>\r</a><c>\r<d>\r</d></c>\r\r\r</xml>\r\r').should.equal('<xml><a><b></b></a><c><d></d></c></xml>');
+	});
+	it('removes vertical tab only whitespace', function () {
+		gobble('\v<xml>\v<a>\v<b>\v</b>\v</a><c>\v<d>\v</d></c>\v\v\v</xml>\v\v').should.equal('<xml><a><b></b></a><c><d></d></c></xml>');
+	});
+	it('removes form feed only whitespace', function () {
+		gobble('\f<xml>\f<a>\f<b>\f</b>\f</a><c>\f<d>\f</d></c>\f\f\f</xml>\f\f').should.equal('<xml><a><b></b></a><c><d></d></c></xml>');
+	});
+	it('removes a mixture of whitespaces', function () {
+		gobble('\f<xml>\v<a>\f<b>\r</b>\f</a><c>\f<d>\n</d></c>\f\t\n</xml>\v\f').should.equal('<xml><a><b></b></a><c><d></d></c></xml>');
+	});
+	it('removes a mixture of whitespaces', function () {
+		gobble('\f<xml>\v<a>\f<b>\r</b>\f</a><c>\f<d>\n</d></c>\f\t\n</xml>\v\f').should.equal('<xml><a><b></b></a><c><d></d></c></xml>');
+	});
+	it('removes whitespace from a sample header', function () {
+		var sample = '<soapenv:Header>		<serviceHeaderV120 xmlns="http://xml.bnz.co.nz/bnzintegrationcommon/BNZServiceHeaderV120">			<user>    <channel>PCIB</channel>        <id idType="AccessId"/>                <subId/>                    </user>    <application>    <id/>             </application>		</serviceHeaderV120>	</soapenv:Header>';
+		var expected = '<soapenv:Header><serviceHeaderV120 xmlns="http://xml.bnz.co.nz/bnzintegrationcommon/BNZServiceHeaderV120"><user><channel>PCIB</channel><id idType="AccessId"/><subId/></user><application><id/></application></serviceHeaderV120></soapenv:Header>';
+		gobble(sample).should.equal(expected);
+	});
 });
