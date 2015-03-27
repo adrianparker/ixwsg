1547146bc52b3e93f064af64bddff7b98742a2ed
Adrian Parker <adrian_parker@bnz.co.nz>
2015-03-27 16:55:07 +1300

    Fix bug parsing large XML documents; + test; increment version

--- index.js
+++ index.js
@@ -6,11 +6,10 @@
  * you may experience data loss. But you wouldn't do that now would you. No. No you would not.
  *
  * Created by Adrian Parker on 3/10/14.
- *
  */
 module.exports = {
 	gobble: function (xml) {
-		var outXml = '',
+		var outXml = [],
 			lastGreaterThanIndex = -1,
 			firstGreaterThanIndex = -1,
 			subsequentLessThanIndex = -1,
@@ -18,7 +17,7 @@ module.exports = {
 			possibleWhitespace = '';
 
 		// remove any leading or trailing chars indiscriminately
-		xml = String(xml).substring(xml.indexOf('<'));
+		xml = xml.substring(xml.indexOf('<'));
 		lastGreaterThanIndex = xml.lastIndexOf('>');
 		if (lastGreaterThanIndex > -1) {
 			xml = xml.substring(0, lastGreaterThanIndex) + '>';
@@ -36,20 +35,21 @@ module.exports = {
 				xml = xml.substring(0, firstGreaterThanIndex + 1) + xml.substring(subsequentLessThanIndex);
 				trimmedGap = subsequentLessThanIndex - (firstGreaterThanIndex + 1);
 			}
-			outXml += xml.substring(0, subsequentLessThanIndex - trimmedGap);
+			outXml.push(new Buffer(xml.substring(0, subsequentLessThanIndex - trimmedGap)));
 			xml = xml.substring(subsequentLessThanIndex - trimmedGap);
 			firstGreaterThanIndex = getLeft(xml);
 			subsequentLessThanIndex = getRight(xml, firstGreaterThanIndex);
 		}
 
 		function getLeft(xml) {
-			return String(xml).indexOf('>');
+			return xml.indexOf('>');
 		}
 
 		function getRight(xml, endLeft) {
-			return String(xml).substring(endLeft).indexOf('<') + endLeft;
+			return xml.substring(endLeft).indexOf('<') + endLeft;
 		}
 
-		return outXml + xml;
+		outXml.push(new Buffer(xml));
+		return Buffer.concat(outXml).toString();
 	}
 }
